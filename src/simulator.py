48210fac131376060752898d688bb25eab8f5bb8
Anagha Jamthe <ajamthe@anagha-jamthe.lan>
2020-05-26 15:37:56 -0500

    repo cleaned and added template and channel creation

--- src/simulator.py
+++ src/simulator.py
@@ -9,35 +9,8 @@ from random import randint
 import random
 import time
 
-# get the logger instance -
-from common.logs import get_logger
-logger = get_logger(__name__)
 
-
-
-
-def download_measurements_chords(instrument_id, start="", end="", format="json"):
-    myfile = open('timelog.txt', 'a')
-    path = "/instruments/" + str(instrument_id) +'.csv'
-  # "api/v1/data.json";
-    chords_uri = "http://c002.rodeo.tacc.utexas.edu:31108" + path;
-    getData = {'email': 'admin@chordsrt.com',
-               'api_key': 'LPYSixTTM3BRcreZE2pP'}
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    res = requests.get(chords_uri, data=getData, headers=headers, verify=False)
-    time = res.elapsed.total_seconds()
-    print(time)
-    if (res.status_code == 200):
-        message = "Measurement found"
-    else:
-        message='Measurements not found'
-    logger.debug(message)
-    logger.debug(res)
-    return (res.content.decode('utf-8'))
-
-
+# Create measurements using the Streams API
 def create_measurements_streams():
     log_file = 'timelog.txt'
     total_time = 0.0
@@ -46,7 +19,7 @@ def create_measurements_streams():
         datetime_now = datetime.now().isoformat()
         start = time.time()
         result = t.streams.create_measurement(inst_id='Ohio_River_Robert_C_Byrd_Locks',
-                                              vars=[{"var_id": "temp", "value": randint(10, 100)},
+                                              vars=[{"var_id": "temp", "value": randint(80, 100)},
                                                     {"var_id": "spc", "value": randint(240, 300)},
                                                     {"var_id": "turb", "value": randint(10, 19)},
                                                     {"var_id": "ph", "value": randint(1, 10)},
@@ -55,17 +28,18 @@ def create_measurements_streams():
         elapsed = time.time() - start
         total_time = round(elapsed, 2) + total_time
         myfile.write(str(i) + ":" + str(round(elapsed, 2)) + "\n")
-        logger.debug("Measurement creation")
-        logger.debug(result)
+        print("Measurement creation")
+       # print(result)
     print(total_time/1000.0)
     myfile.close()
 
 
+# Download measurements with Streams API
 def download_measurements_streams():
     log_file = 'timelog.txt'
     total_time = 0.0
     myfile = open('timelog.txt', 'a')
-    for i in range(0, 100):
+    for i in range(0, 500):
         datetime_now = datetime.now().isoformat()
         start = time.time()
         ## We can include a start_date and end_date in this call to list measurements
@@ -74,9 +48,9 @@ def download_measurements_streams():
         elapsed = time.time() - start
         total_time = round(elapsed, 2) + total_time
         myfile.write(str(i) + ":" + str(round(elapsed, 2)) + "\n")
-        logger.debug("Measurements fetch")
-        logger.debug(result)
-    print(total_time/100.0)
+        print("Measurements fetch")
+       # print(result)
+    print(total_time/500.0)
     myfile.close()
 
 
@@ -91,61 +65,78 @@ def create_project_site_inst_var():
     ## Create Site
     result, debug = t.streams.create_site(project_uuid='wq_demo_project',site_name='wq_demo_site', site_id='wq_demo_site',
                                           latitude=50, longitude = 10, elevation=2,description='test_site', _tapis_debug=True)
-    logger.debug("site creation")
-    logger.debug(result)
+    print("site creation")
+    print(result)
 
     ## Create Instruments
     result, debug = t.streams.create_instrument(project_uuid='wq_demo_project',topic_category_id ='2',site_id='wq_demo_site',
                                                 inst_name='Ohio_River_Robert_C_Byrd_Locks',inst_description='demo instrument',
                                                 inst_id='Ohio_River_Robert_C_Byrd_Locks', _tapis_debug=True)
-    logger.debug("Instrument creation")
-    logger.debug(result)
+    print("Instrument creation")
+    print(result)
 
     ## Create Variables
     result, debug = t.streams.create_variable(project_uuid='wq_demo_project',topic_category_id ='2',
                                               site_id='wq_demo_site',inst_id='Ohio_River_Robert_C_Byrd_Locks',
                                               var_name='temperature', shortname='temp',var_id='temp', _tapis_debug=True)
-    logger.debug("Temperature Variable creation")
-    logger.debug(result)
+    print("Temperature Variable creation")
+    print(result)
 
     ## Create Variables
     result, debug = t.streams.create_variable(project_uuid='wq_demo_project', topic_category_id='2',
                                               site_id='wq_demo_site', inst_id='Ohio_River_Robert_C_Byrd_Locks',
                                               var_name='battery', shortname='bat', var_id='batv',
                                               _tapis_debug=True)
-    logger.debug("Battery Variable creation")
-    logger.debug(result)
+    print("Battery Variable creation")
+    print(result)
 
     ## Create Variables
     result, debug = t.streams.create_variable(project_uuid='wq_demo_project', topic_category_id='2',
                                               site_id='wq_demo_site', inst_id='Ohio_River_Robert_C_Byrd_Locks',
                                               var_name='specific_conductivity', shortname='spc', var_id='spc',
                                               _tapis_debug=True)
-    logger.debug("Specific conductivity Variable creation")
-    logger.debug(result)
+    print("Specific conductivity Variable creation")
+    print(result)
 
     ## Create Variables
     result, debug = t.streams.create_variable(project_uuid='wq_demo_project', topic_category_id='2',
                                               site_id='wq_demo_site', inst_id='Ohio_River_Robert_C_Byrd_Locks',
                                               var_name='turbidity', shortname='turb', var_id='turb',
                                               _tapis_debug=True)
-    logger.debug("Turbidity Variable creation")
-    logger.debug(result)
+    print("Turbidity Variable creation")
+    print(result)
 
     ## Create Variables
     result, debug = t.streams.create_variable(project_uuid='wq_demo_project', topic_category_id='2',
                                               site_id='wq_demo_site', inst_id='Ohio_River_Robert_C_Byrd_Locks',
                                               var_name='ph_level', shortname='ph', var_id='ph',
                                               _tapis_debug=True)
-    logger.debug("Ph Variable creation")
-    logger.debug(result)
+    print("Ph Variable creation")
+    print(result)
 
-## Enter the username and password for test accounts on dev
+## Create templates and channels
+def create_template():
+    result,debug = t.streams.create_template(template_id='demo_wq_data_test_template3', type='stream', script='var period=5s\n '
+                    'var every=0s\n var crit lambda \n var channel_id string\n stream\n    |from()\n        .measurement(\'tsdata\')\n        '
+                    ' .groupBy(\'var\')\n     |window()\n        .period(period)\n         .every(every)\n         .align()\n     |alert()\n       '
+                    ' .id(channel_id +  \' {{ .Name }}/{{ .Group }}/{{.TaskName}}/{{index .Tags \"var\" }}\')\n         .crit(crit)\n       '
+                    '  .message(\'{{.ID}} is {{ .Level}} at time: {{.Time}} as value: {{ index .Fields \"value\" }} exceeded the threshold\')\n       '
+                    ' .details(\'\')\n         .post()\n         .endpoint(\'api-alert\')\n     .captureResponse()\n    |httpOut(\'msg\')', _tapis_debug=True)
+
+    # Actor id=7kxayXzzX63oY, nonce= TACC-PROD_Xr3LW5MOZXGkv, maxUses =100
+
+def create_channel():
+    result,debug =  t.streams.create_channels(channel_id='demo_wq_test_3', channel_name='demo.wq.test', template_id='demo_wq_data_test_template3',
+                                              triggers_with_actions=[{"inst_ids":"Ohio_River_Robert_C_Byrd_Locks","condition":{"key":"Ohio_River_Robert_C_Byrd_Locks.temp","operator":">", "val":"90"}, "action":{"method":"ACTOR"," actor_id":"7kxayXzzX63oY","message":"Instrument: Ohio_River_Robert_C_Byrd_Locks temp exceeded threshold", "abaco_base_url":"https://api.tacc.utexas.edu","nonces":"TACC-PROD_1kqBLDGaxxJY3"}}]
+                                              ,_tapis_debug=True)
+
 if __name__ == "__main__":
     t = DynaTapy(base_url='https://dev.develop.tapis.io', username='testuser6', account_type='user', password='testuser6', tenant_id='dev')
     t.get_tokens()
     #create_project_site_inst_var()
-    #create_measurements_streams()
-    download_measurements_streams()
+    #create_template()
+    #create_channel()
+    create_measurements_streams()
+    #download_measurements_streams()
     #download_measurements_chords(9)
 
